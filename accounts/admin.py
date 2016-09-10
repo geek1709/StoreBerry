16d6257bb27a668b13dba0704a415e5bb875e8ef
Imcber <imcber1@gmail.com>
2016-09-10 11:30:55 -0500

    Modelos para tienda y usuarios

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UserComprador,UserTienda
 
-# Register your models here.
+admin.site.register(UserTienda)
+admin.site.register(UserComprador)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserTienda(models.Model):
+	nombre_tienda = models.CharField(max_length=140)
+	user = models.OneToOneField(User)
+
+	def __str__(self):
+		return 'esta tienda le pertenece a {}'.format(self.user)
+
+class UserComprador(models.Model):
+	GENEROS = (
+		('Hombre','Hombre'),
+		('Mujer', 'Mujer'),
+		)
+	fecha_nacimiento = models.DateField(null=True,blank=True)
+	sexo = models.CharField(max_length=140,choices=GENEROS, default="Mujer")
+	user = models.OneToOneField(User)
+
+	def __str__(self):
+		return 'este perfil le pertenece a {}'.format(self.user)
